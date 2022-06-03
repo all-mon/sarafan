eed2838cd1469455d66f692881eb1f4f17a62046
monah <m0n7h0v@gmail.com>
2022-06-03 17:21:28 +0500

    p.6 setting up Set up Security/oauth2/vue

--- src/main/java/com/forhh/sarafan/config/WebSecurityConfig.java
+++ src/main/java/com/forhh/sarafan/config/WebSecurityConfig.java
@@ -19,9 +19,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .antMatcher("/**")
                 .authorizeRequests()
-                .mvcMatchers("/").permitAll()
-                .anyRequest().authenticated()
+                .antMatchers("/","/login**","/js/**","/error**")//разрешить запросы по текущим адрессам
+                .permitAll()//для остальных
+                .anyRequest()//любых запросов
+                .authenticated()//просим авторизоваться
+                .and().logout().logoutSuccessUrl("/").permitAll()
                 .and()
                 .csrf().disable();
     }
